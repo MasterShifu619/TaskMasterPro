9e7d0859ac9fa452e295c0cb2ccc3d3a9a58c76f
IshaDave26 <ishvdave@gmail.com>
2024-11-24 15:18:01 -0500

    Add test cases for task suggestion function
    Added test cases to test task suggestion and its use cases.

--- todo/tests/test_suite.py
+++ todo/tests/test_suite.py
@@ -1,11 +1,13 @@
 import unittest
 from todo.tests.test_user_analytics import UserAnalyticsTest
+from todo.tests.test_task_suggester import TaskSuggesterTest
 # from test_send_due_tasks_email import SendDueTasksEmailTest
 # from test_login_request import LoginRequestTest
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(UserAnalyticsTest))
+    suite.addTest(unittest.makeSuite(TaskSuggesterTest))
     # suite.addTest(unittest.makeSuite(SendDueTasksEmailTest))
     # suite.addTest(unittest.makeSuite(LoginRequestTest))
     return suite
